4548a1ee7932530c05a2baa258bd67046133e667
Kaikaiser <2524124638@qq.com>
2025-09-15 17:23:18 +0800

    epollpoller编写

--- EPollPoller.cpp
+++ EPollPoller.cpp
@@ -1,8 +1,13 @@
 #include "EPollPoller.h"
 #include "Logger.h"
+#include "Channel.h"
+
 #include<errno.h>
-const int kNew = -1;
+// channel未添加到poller中 
+const int kNew = -1;  // channel的初始化成员index_ 也为-1
+// channel已添加到poller中
 const int kAdded = 1;
+// channel从poller中删除
 const int kDeleted = 2;
 
 EPollPoller::EPollPoller(EventLoop *loop) 
@@ -15,7 +20,95 @@ EPollPoller::EPollPoller(EventLoop *loop)
         LOG_FATAL("epoll_create error:%d \n", errno);
     }
 }
-EPollPoller::~EPollPoller()
+
+TimeStamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels)
 {
 
+}
+
+
+// channel update remove => EventLoop updateChannel removeChannel => poller updateChannel removeChannel
+/*
+*                EventLoop
+*   ChannelList           Poller
+*                  ChannnelMap <fd, channel*>   epollfd
+*/
+void EPollPoller::updateChannel(Channel *channel)  // -->eopll_ctl
+{
+    const int index = channel->index();
+    LOG_INFO("fd=%d events=%d index=%d \n", channel->fd(), channel_>events(), channel->index());
+    if(index ==kNew || index == kDeleted)
+    {
+        if(index == kNew)
+        {
+            int fd = channel->fd();
+            channels_[fd] = channel;
+        }
+        channel->set_index(kAdded);
+        update(EPOLL_CTL_ADD, channel);
+    }
+    else  //表示channel已经在poller上注册过了
+    {
+        int fd = channel->fd();
+        if(channel->isNoneEvent())
+        {
+            update(EPOLL_CTL_DEL, channel);
+            channel->set_index(kDeleted);
+        }
+        else
+        {
+            update(EPOLL_CTL_MOD, channel);
+        }
+    }
+}
+
+// 从poller中删除channel 1.从poller的channelMap中删除 2.从epoll中删除
+void EPollPoller::removeChannel(Channel *channel)  // -->eopll_ctl
+{
+    // 从poller的channelMap中删除
+    int fd = channel->fd();
+    channels_.erase(fd);
+    // 从epoll中删除
+    int index = channel->index();
+    if(index == kAdded)
+    {
+        update(EPOLL_CTL_DEL, channel);
+
+    }
+    channel->set_index(kNew);
+}
+
+void EPollPoller::fillActiveChannels(int numEvents, ChannelList *activeChannels) const;
+// 更新channel通道 epoll_ctl_(add/del/mod)
+void EPollPoller::update(int operation, Channel *channel)
+{
+    epoll_event event;
+    memset(&event, 0, sizeof(event));
+
+    int fd = channel->fd();
+    event.events = channel->events();
+    event.data.fd = fd;
+    event.data.ptr = channel;
+    int fd = channel->fd();
+    if(::epoll_ctl(epollfd_, operation, fd, &event)<0)
+    {
+        if(operation == EPOLL_CTL_DEL)
+        {
+            if(operation == EPOLL_CTL_DEL)
+            {
+                LOG_ERROR("epoll_ctl_del error:%d\n", errno);
+            }
+            else
+            {
+                LOG_FATAL("epoll_ctl_add/mod error:%d\n", errno);
+            }
+        }
+    }
+
+}
+
+
+EPollPoller::~EPollPoller()
+{
+    ::close(epollfd_);
 }
--- Poller.h
+++ Poller.h
@@ -16,9 +16,19 @@ public:
     Poller(EventLoop *loop);
     virtual ~Poller();
 
+    // 给所有的io复用保留统一的接口
     virtual TimeStamp poll(int timeoutMs, ChannelList *activeChannels) = 0;
+    virtual void updateChannel(Channel *channel) = 0;
+    virtual void removeChannel(Channel *channel) = 0;
+    // 判断参数channel是否在当前poller中
+    bool hasChannel(Channel *channel) const;
+
+    // EventLoop 可以通过该接口获取默认的io复用的具体实现
+    static Poller* newDefaultPoller(EventLoop *loop);
 protected:
+    // 这里面的key -> sockfd   value -> sockfd对应的channel类型 
     using ChannelMap = std::unordered_map<int, Channel*>;
+    ChannelMap channels_;
 private:
     EventLoop *ownerLoop_; // 定义Poller所属的事件循环EventLoop
 
