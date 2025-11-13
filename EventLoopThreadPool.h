beddcc76f215c07c1479bd18fec6d3d53a062eb1
Kaibaba <2524124638@qq.com>
2025-11-14 00:21:17 +0800

    1114

--- EventLoopThreadPool.h
+++ EventLoopThreadPool.h
@@ -1,11 +1,25 @@
 #pragma once 
 #include "noncopyable.h"
+#include <functional>
+#include <string>
+#include <vector>
+#include <memory>
 
-
+class EventLoopThread;
 class EventLoop;
+
 class EventLoopThreadPool : noncopyable
 {
 public:
+    using ThreadInitFunc = std::function<void(EventLoop*)>;
 
 private:
+    EventLoop *baseLoop_;
+    std::string name_;  // EventLoop loop;
+    bool started_;
+    int numThreads_;
+    int next_;
+    std::vector<std::unique_ptr<EventLoopThread*>> threads_;
+    std::vector<EventLoop*> loops_;
+
 }
