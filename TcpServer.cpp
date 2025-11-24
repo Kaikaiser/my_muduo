dd0fc776517d617dbfc990bc70269d63ef576017
Kaikaiser <2524124638@qq.com>
2025-11-24 17:26:59 +0800

    Tcp1124

--- TcpServer.cpp
+++ TcpServer.cpp
@@ -1,2 +1,18 @@
 #include "TcpServer.h"
+#include "Logger.h"
 
+EventLoop* CheckLoopNotNull(EventLoop *loop)
+{
+    if(loop == nullptr)
+    {
+        LOG_FATAL("%s: %s: %d mainloop does not exist! \n", __FILE__, __func__, __LINE__);
+    }
+    return loop;
+}
+
+
+TcpServer::TcpServer(EventLoop *loop, const InetAddress &listenAddr, Option option = kNOReusePort)
+    : loop_(CheckLoopNotNull(loop))
+    , inPort_(listenAddr.toIpPort())
+
+~TcpServer();
