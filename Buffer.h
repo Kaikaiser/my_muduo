4635364923bac20a3e168c9f68dc0eb203c70224
Kaikaiser <2524124638@qq.com>
2025-11-28 08:57:12 +0800

    1128

--- Buffer.h
+++ Buffer.h
@@ -1,6 +1,15 @@
 #pragma once 
-
+#include <vector>
+// 网络库底层的缓冲区类型的定义
 class Buffer
 {
+public:
+    static const size_t kCheapPrepend = 0;
+    static const size_t kInitialSize = 1024;
+
+private:
+    std::vector<char> buffer_;
+    size_t readerIndex_;
+    size_t writerIndex_;
 
 };
--- TcpConnection.h
+++ TcpConnection.h
@@ -5,7 +5,7 @@
 class TcpConnection : noncopyable
 {
 public:
-
+    
 private:
 
 };
