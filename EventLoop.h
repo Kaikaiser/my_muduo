42940b6f4c5c052fcfaaf657af8b0f84e9ee80e9
Kaikaiser <2524124638@qq.com>
2025-12-22 17:30:10 +0800

    11

--- EventLoop.h
+++ EventLoop.h
@@ -69,6 +69,6 @@ private:
     Channel *currentActiveChannel_;
 
     std::atomic_bool callingPendingFunctors_; // 标识当前loop是否有需要执行的回调操作
-    std::vector<Functor> pendingFunctors_; // 存储loop需要执行的所有回调操作
+    std::vector<Functor> pendingFunctors_; // 存储loop需要执行的所有回调操作(当线程不和loop对应时)
     std::mutex mutex_; // 互斥锁，用来保护上面vector容器的线程安全操作
 };  
